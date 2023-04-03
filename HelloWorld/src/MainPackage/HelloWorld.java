afd84fd6a49b18761ba0f5f0d24b77026dd31a54
Rafael Bueno <rafaelbueno.rgb@gmail.com>
2023-04-03 19:58:34 -0300

    Update HelloWorld.java

--- HelloWorld/src/MainPackage/HelloWorld.java
+++ HelloWorld/src/MainPackage/HelloWorld.java
@@ -6,7 +6,6 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello Brazil!");
 		System.out.println("Hello Curitiba!");
-		System.out.println("Rafael de Goes Bueno");
 
 	}
 
