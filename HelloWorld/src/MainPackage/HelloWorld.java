3001c54ff3565e5c10e28de9cb797ed4d444354c
Yasmin Senkiv Cruz - 172114600 <172114600@ulife.com.br>
2023-04-03 20:08:25 -0300

    Yasmin

--- HelloWorld/src/MainPackage/HelloWorld.java
+++ HelloWorld/src/MainPackage/HelloWorld.java
@@ -3,10 +3,11 @@ package MainPackage;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Hello Brazil!");
-		System.out.println("Hello Curitiba!");
-
+	System.out.println("Hello World");
+	System.out.println("Hello Brasil");
+	System.out.println("Hello Curitiba");
+	System.out.println("Yasmin");
+	
 	}
 
 }
