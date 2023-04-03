b743b65e81526ecd11ab15160ba26642d477d8d7
Rafael de Goes Bueno - 172121776 <172121776@ulife.com.br>
2023-04-03 19:51:51 -0300

    Imprima meu Nome;

--- HelloWorld/src/MainPackage/HelloWorld.java
+++ HelloWorld/src/MainPackage/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello Brazil!");
 		System.out.println("Hello Curitiba!");
+		System.out.println("Rafael Bueno");
 
 	}
 
